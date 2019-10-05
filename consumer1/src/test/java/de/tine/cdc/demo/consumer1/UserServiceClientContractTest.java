ce45cba133c009225c3d50389d0a926b9bb4da31
Kristine Jetzke <kristine.jetzke@kreuzwerker.de>
2019-10-05 14:37:55 +0200

    Upgrade pact-jvm to 4.0.0

--- consumer1/src/test/java/de/tine/cdc/demo/consumer1/UserServiceClientContractTest.java
+++ consumer1/src/test/java/de/tine/cdc/demo/consumer1/UserServiceClientContractTest.java
@@ -1,10 +1,10 @@
 package de.tine.cdc.demo.consumer1;
 
-import au.com.dius.pact.consumer.Pact;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
-import au.com.dius.pact.model.RequestResponsePact;
+import au.com.dius.pact.core.model.RequestResponsePact;
+import au.com.dius.pact.core.model.annotations.Pact;
 import io.pactfoundation.consumer.dsl.LambdaDsl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
