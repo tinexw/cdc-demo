ba930b80fa6d5600260c13f53124b5ac02ed216d
Kristine Jetzke <kristine.jetzke@kreuzwerker.de>
2020-08-27 17:34:24 +0200

    Upgrade libraries

--- user-service/src/test/java/de/tine/cdc/demo/userservice/ContractTestFinal.java
+++ user-service/src/test/java/de/tine/cdc/demo/userservice/ContractTestFinal.java
@@ -1,10 +1,10 @@
 package de.tine.cdc.demo.userservice;
 
-import au.com.dius.pact.provider.junit.Provider;
-import au.com.dius.pact.provider.junit.loader.PactFolder;
 import au.com.dius.pact.provider.junit5.HttpTestTarget;
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
