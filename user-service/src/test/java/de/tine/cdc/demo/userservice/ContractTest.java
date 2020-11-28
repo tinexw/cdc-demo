49f82dc3120b9c77ac74f4e00fcbf4263dd46055
Kristine Jetzke <kristine.jetzke@kreuzwerker.de>
2020-11-28 10:15:42 +0100

    Switch to maven for testing issue https://github.com/pact-foundation/pact-jvm/issues/1255

---
+++ user-service/src/test/java/de/tine/cdc/demo/userservice/ContractTest.java
@@ -0,0 +1,34 @@
+package de.tine.cdc.demo.userservice;
+
+import au.com.dius.pact.provider.junit5.HttpTestTarget;
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@Provider("user-service")
+@PactFolder("pacts")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ContractTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void before(PactVerificationContext context) {
+        context.setTarget(new HttpTestTarget("localhost", port));
+    }
+
+    @TestTemplate
+    @ExtendWith(PactVerificationInvocationContextProvider.class)
+    void pactVerificationTestTemplate(PactVerificationContext context) {
+        context.verifyInteraction();
+    }
+
+}
+
